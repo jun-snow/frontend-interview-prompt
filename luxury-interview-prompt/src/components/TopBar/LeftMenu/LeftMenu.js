a7327d98c13bd2e0c653cc756e700aa6b5844fb2
Jun Kelly <kellyja4@gmail.com>
2019-07-20 16:06:20 -0700

    Added container

--- luxury-interview-prompt/src/components/TopBar/LeftMenu/LeftMenu.js
+++ luxury-interview-prompt/src/components/TopBar/LeftMenu/LeftMenu.js
@@ -4,17 +4,17 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuRounded from '@material-ui/icons/MenuRounded';
 
 const LeftMenu = () => (
-  <>
+  <div className={styles.leftMenuContainer}>
     <div className={styles.mysteryRectangle} />
     <IconButton
-      className={styles.searchMenu}
+      className={styles.leftMenu}
       edge="start"
       color="inherit"
       aria-label="Open drawer"
     >
       <MenuRounded />
     </IconButton>
-  </>
+  </div>
 );
 
 export default LeftMenu;
