a780ef8523ae4be5e0f846b60211b20a92e59b20
Jun Kelly <kellyja4@gmail.com>
2019-07-20 10:46:51 -0700

    Initial environment setup with Create React App and folder structure established

---
+++ luxury-interview-prompt/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      test
+    </div>
+  );
+}
+
+export default App;
---
+++ luxury-interview-prompt/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.module.css';
+import App from './components/App/';
+
+render(<App />, document.getElementById('root'));
