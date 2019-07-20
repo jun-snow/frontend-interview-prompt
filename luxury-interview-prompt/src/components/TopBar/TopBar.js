f9c4986b8485554c746e688a39cb9b24ff376599
Jun Kelly <kellyja4@gmail.com>
2019-07-20 15:28:05 -0700

    Topbar component structure

--- luxury-interview-prompt/src/components/TopBar/TopBar.js
+++ luxury-interview-prompt/src/components/TopBar/TopBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './TopBar.module.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import LeftMenu from './LeftMenu';
 
 const TopBar = () => {
   return (
-    <div className={styles.topBarContainer}>
-      topbar
+    <div id={styles.topBar}>
+      <AppBar
+        position="fixed"
+        className={styles.appBarContainer}
+      >
+        <ToolBar className={styles.toolBar} disableGutters>
+          <LeftMenu />
+        </ToolBar>
+      </AppBar>
     </div>
   );
 };
