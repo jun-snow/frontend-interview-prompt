f8ba2048a08348db612a7b9395fe5c3f220336c2
Jun Kelly <kellyja4@gmail.com>
2019-07-20 22:23:31 -0700

    Installed FontAwesome just for the gem icon

--- luxury-interview-prompt/src/components/SideBar/MenuLayout/MenuLayout.js
+++ luxury-interview-prompt/src/components/SideBar/MenuLayout/MenuLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import KeyboardArrowRightRounded from '@material-ui/icons/KeyboardArrowRightRounded';
-import BarChart from '@material-ui/icons/BarChart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGem } from '@fortawesome/free-regular-svg-icons';
 import MenuRounded from '@material-ui/icons/MenuRounded';
 import FileCopy from '@material-ui/icons/FileCopy';
 import Settings from '@material-ui/icons/Settings';
@@ -9,10 +10,11 @@ import styles from '../SideBar.module.css';
 const MenuLayout = () => (
   <div className={styles.sideBarGroup}>
     <div
+      style={{ marginLeft: "-1px"}}
       className={styles.sideBarItem}
       tabIndex="0"
     >
-      <BarChart />
+      <FontAwesomeIcon icon={faGem} size="lg"/>
       <span className={styles.sideBarText}>
         UI Elements
       </span>
