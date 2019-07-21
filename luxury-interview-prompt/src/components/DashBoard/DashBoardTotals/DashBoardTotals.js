5a18eef5e051fa7cc3d4f8789da428e205027cc2
Jun Kelly <kellyja4@gmail.com>
2019-07-20 20:10:24 -0700

    Margin, pdading, display adjustments

--- luxury-interview-prompt/src/components/DashBoard/DashBoardTotals/DashBoardTotals.js
+++ luxury-interview-prompt/src/components/DashBoard/DashBoardTotals/DashBoardTotals.js
@@ -4,30 +4,32 @@ import DataColumn from './DataColumn';
 
 const DashBoardTotals = () => (
   <div className={styles.totalsContainer}>
-    <div className={styles.totalsHeader}>
-      <h2 className={styles.totalsTitle}>
-        Totals
-      </h2>
-      <p className={styles.totalsDescription}>
-        Last Week
-      </p>
-    </div>
-    <div className={styles.totalsData}>
-      <DataColumn numbers={'597'}>
-        New Feedbacks
-      </DataColumn>
-      <div className={styles.totalsDivider} />
-      <DataColumn numbers={'16.6M$'}>
-        Total Profit
-      </DataColumn>
-      <div className={styles.totalsDivider} />
-      <DataColumn numbers={'7.5K'}>
-        New Orders
-      </DataColumn>
-      <div className={styles.totalsDivider} />
-      <DataColumn numbers={'+48%'}>
-        Brand Popularity
-      </DataColumn>
+    <div className={styles.totalsInnerContainer}>
+      <div className={styles.totalsHeader}>
+        <h2 className={styles.totalsTitle}>
+          Totals
+        </h2>
+        <p className={styles.totalsDescription}>
+          Last Week
+        </p>
+      </div>
+      <div className={styles.totalsData}>
+        <DataColumn numbers={'597'}>
+          New Feedbacks
+        </DataColumn>
+        <div className={styles.totalsDivider} />
+        <DataColumn numbers={'16.6M$'}>
+          Total Profit
+        </DataColumn>
+        <div className={styles.totalsDivider} />
+        <DataColumn numbers={'7.5K'}>
+          New Orders
+        </DataColumn>
+        <div className={styles.totalsDivider} />
+        <DataColumn numbers={'+48%'}>
+          Brand Popularity
+        </DataColumn>
+      </div>
     </div>
   </div>
 );
