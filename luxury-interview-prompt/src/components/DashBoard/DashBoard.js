e2714a321e6dfb205b51834a10632e272931dd2b
Jun Kelly <kellyja4@gmail.com>
2019-07-20 21:23:42 -0700

    Placed footer and slightly rearranged layout

--- luxury-interview-prompt/src/components/DashBoard/DashBoard.js
+++ luxury-interview-prompt/src/components/DashBoard/DashBoard.js
@@ -3,16 +3,20 @@ import styles from './DashBoard.module.css';
 import DashBoardHeader from './DashBoardHeader';
 import DashBoardTotals from './DashBoardTotals';
 import DashBoardGraphs from './DashBoardGraphs';
+import DashBoardFooter from './DashBoardFooter';
 
 const DashBoard = () => {
   return (
-    <main id={styles.container}>
-      <DashBoardHeader />
-      <div className={styles.panelContainer}>
-        <DashBoardTotals />
-        <DashBoardGraphs />
-      </div>
-    </main>
+    <div id={styles.container}>
+      <main className={styles.mainContainer}>
+        <DashBoardHeader />
+        <div className={styles.panelContainer}>
+          <DashBoardTotals />
+          <DashBoardGraphs />
+        </div>
+      </main>
+      <DashBoardFooter />
+    </div>
   );
 }
 
