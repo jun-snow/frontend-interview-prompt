6caec9f57ff0b4f815acae617fa5971b626f0125
Jun Kelly <kellyja4@gmail.com>
2019-07-22 10:22:41 -0700

    Added setings button

--- luxury-interview-prompt/src/components/DashBoard/DashBoard.js
+++ luxury-interview-prompt/src/components/DashBoard/DashBoard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './DashBoard.module.css';
+import SettingsRounded from '@material-ui/icons/SettingsRounded';
 import DashBoardHeader from './DashBoardHeader';
 import DashBoardTotals from './DashBoardTotals';
 import DashBoardGraphs from './DashBoardGraphs';
@@ -10,6 +11,10 @@ const DashBoard = () => {
     <div id={styles.container}>
       <main className={styles.mainContainer}>
         <DashBoardHeader />
+        <SettingsRounded
+          className={styles.settingsIcon}
+          role="button"
+        />
         <div className={styles.panelContainer}>
           <DashBoardTotals />
           <DashBoardGraphs />
